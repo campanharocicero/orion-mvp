64fb6d260c6b149de24658fbf3bb5b5a6713c278
campanharocicero <campanharocicero@gmail.com>
2026-04-27 10:47:40 -0300

    Add express.json() middleware for body parsing
    Required to parse JSON request bodies for the webhook endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/', (req,res) => res.send('OK ROOT'));
 app.get('/health', (req,res) => res.send('OK HEALTH'));
