d4a1208442fd393b0da235257808b0e036dad7b4
campanharocicero <campanharocicero@gmail.com>
2026-04-27 00:43:21 -0300

    Test minimal server for diagnostics

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
-require('dotenv').config();
-
 const app = express();
-app.use(express.json());
 
-// Log all requests
-app.use((req,res,next)=>{ console.log(req.method + ' ' + req.url); next(); });
-
-app.get('/', (req, res) => { console.log('GET / reached'); res.status(200).send('ORION ONLINE'); });
-
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');
-        });
+app.get('/', (req,res) => res.send('OK ROOT'));
+app.get('/health', (req,res) => res.send('OK HEALTH'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-      console.log('ORION running on ' + PORT);
+const server = app.listen(PORT, '0.0.0.0', () => {
+    console.log('LISTENING ON ' + PORT);
 });
+
+server.on('error', (err) => {
+    console.error('SERVER ERROR:', err);
+});
+
+process.on('uncaughtException', err => console.error(err));
+process.on('unhandledRejection', err => console.error(err));
