27b54d4c808bf423b2197d9d085b8b47bafc9a87
campanharocicero <campanharocicero@gmail.com>
2026-04-27 00:03:55 -0300

    Simplify health check endpoint and add request logging

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+// Log all requests
+app.use((req,res,next)=>{ console.log(req.method + ' ' + req.url); next(); });
+
 app.get('/', (req, res) => {
       res.json({status: 'ok', message: 'ORION Backend'});
 });
 
 app.get('/health', (req, res) => {
-      res.json({status: 'ok'});
-});
+  res.status(200).send('OK');
+        });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
